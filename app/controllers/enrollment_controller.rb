ca1a28e61fa05e61dba20dabab74bf96daa3a6c0
Mihndim <jelsieprisca2016@gmail.com>
2021-05-03 08:53:10 -0400

    Add enrollment controller before action and create method

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -1,2 +1,15 @@
 class EnrollmentController < ApplicationController
+  before_action :require_login
+  def create
+    event = Event.find(params[:event_id])
+    enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
+    if enrollment.save
+      enrollment.invited!
+      flash[:notice] = "Invitation sent!"
+      redirect_to users_path(event_id: event.id)
+    else
+      flash[:alert] = 'Ooops! Something went wrong...'
+      redirect_to event_path(event)
+    end
+  end
 end
