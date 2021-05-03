16a2cb72311954efdf26a6ed4dfef7c473c5b299
errea <erreakezy@gmail.com>
2021-05-03 16:06:09 +0100

    Modify events controller remove extra end

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,30 @@
-def new
-  @event = Event.new
-end
 
-def create
-  @event = current_user.events.build(event_params)
+class EventsController < ApplicationController
+  include EventsHelper
+ before_action :require_login, except: [:index]
 
-  if @event.save
-    flash[:success] = "Event '#{@event.name}' created!"
-    redirect_to @event
-  else
-    flash[:alert] = "Something Happened!"
-    render 'new'
+  def index
+    @events = Event.all
   end
-end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Event '#{@event.name}' created!"
+      redirect_to @event
+    else
+      flash[:alert] = "Something Happened!"
+      render 'new'
+    end
+  end
 
 end
