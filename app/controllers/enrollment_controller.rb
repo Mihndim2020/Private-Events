9ab6d17564d2c584c3bd30b4fcbfb2365b8435a9
Mihndim <jelsieprisca2016@gmail.com>
2021-05-03 08:54:46 -0400

    Add enrollment controller destroy action

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -1,5 +1,7 @@
 class EnrollmentController < ApplicationController
+
   before_action :require_login
+
   def create
     event = Event.find(params[:event_id])
     enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
@@ -12,4 +14,19 @@ class EnrollmentController < ApplicationController
       redirect_to event_path(event)
     end
   end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    enrollment = Enrollment.find(params[:id])
+    if current_user == event.creator
+      enrollment.destroy
+      flash[:notice] = "The invitation is cancelled!"
+    else
+      enrollment.invited!
+      # enrollment.save
+      flash[:notice] = "You have dropped the enrollment for the #{event.name}!"
+    end
+    redirect_to users_path(event_id: event.id, id: enrollment.id)
+  end
+  
 end
