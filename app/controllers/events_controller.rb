5d3548511d94e65f37ddc0d12f054cd909982687
errea <erreakezy@gmail.com>
2021-05-05 01:20:40 +0100

    Review-fix: refactor code in event controller and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @help = User.find(@event.enrollments.accepted.map { |enr| enr.user_id })
   end
 
   def new
